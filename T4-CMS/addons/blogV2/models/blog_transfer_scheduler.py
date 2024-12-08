8798df7c409752970a5a74e7a1cca57f4e5fc28a
HungVP-24 <HungVP@int.t4tek.co>
2024-12-08 22:31:41 +0700

    Update blog_transfer_scheduler.py

--- T4-CMS/addons/blogV2/models/blog_transfer_scheduler.py
+++ T4-CMS/addons/blogV2/models/blog_transfer_scheduler.py
@@ -17,19 +17,3 @@ class BlogTransferScheduler(models.Model):
         string='Chiến dịch chuyển',
     )
    
-
-    @api.model
-    def _run_transfer_jobs(self):
-        """Phương thức được gọi bởi cron job để thực hiện các chiến dịch chuyển"""
-
-        current_time = fields.Datetime.now()
-        schedulers = self.search([])
-
-        for scheduler in schedulers:
-            try:
-                if scheduler.blog_transfer_id.scheduled_date <= current_time:
-                    scheduler.blog_transfer_id.action_start_transfer()
-            except Exception as e:
-                _logger.error(
-                    f"Error processing transfer: {str(e)}")
-                continue
