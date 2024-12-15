c9dd865b6b9d8c81ca498a18e97c0bf42cea3dc6
HungVP-24 <HungVP@int.t4tek.co>
2024-12-15 22:11:35 +0700

    Update __manifest__.py

--- T4-CMS/addons/blogV2/__manifest__.py
+++ T4-CMS/addons/blogV2/__manifest__.py
@@ -1,20 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Blog",
-    "version": "1.0",
+    'version': '1.0.1',
     "category": "Accounting/Payment Providers",
     "sequence": 0,
     "summary": "A Vietnam payment provider.",
-    "description": " ",  # Non-empty string to avoid loading the README file.
-    "author": "",
-    "depends": ["base", "web", "website_blog"],
-    "data": [  # Do no change the order
+    "description": "Module for managing blog transfers, scheduling blog posts, and server configurations for blog management in Odoo.",  # Added description text
+    "author": "",  # You can fill in the author name here if needed
+    "depends": [
+        "base", 
+        "web", 
+        "website_blog"
+    ],
+    "data": [  # Do not change the order
         "security/ir.model.access.csv",
+        "data/cron_job_transfer.xml",
         "views/blog_transfer_scheduler.xml",
-        "data/cron_check_blog_transfer.xml",
         "views/blog_transfer.xml",
         "views/serverView.xml",
         "views/menuItems.xml",
+        "views/blog_transfer_kanban.xml",
     ],
     "installable": True,
     "auto_install": False,
@@ -22,11 +27,9 @@
     "license": "LGPL-3",
 
     'assets': {
-        'web.assets_backend':
-        [
+        'web.assets_backend': [
             'blogV2/static/src/js/*.js',
             'blogV2/static/src/xml/*.xml',
         ]
     }
-
 }
